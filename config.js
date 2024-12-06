75f477a80fdc8d588d3b35f8bf6caba2f3973911
Marcos Filipe Capella <marcoscapella@outlook.com>
2024-12-05 23:24:30 -0300

    Chaves publicadas

---
+++ config.js
@@ -0,0 +1,6 @@
+// Dados sensíveis, publicados para entrega do trabalho
+
+export const config = {
+  applicationId: "tUg59uGQI7MdmGAu9zEynicEP9RAYM88h2ajcjfo",
+  restAPIKey: "T1cDYx0RCP8KPon8PwCgaNLeC52xtg5682BVjLUc",
+};
